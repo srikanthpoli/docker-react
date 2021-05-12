05dab626d9231aa4e6793e1860a10a469f407786
srikanthpoli <sri.polisettys@gmail.com>
2021-05-12 09:03:02 +0530

    AWS push
    AWS push

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-     Sriasdadasd
+     Srikanth in AWS
         </p>
         <a
           className="App-link"
